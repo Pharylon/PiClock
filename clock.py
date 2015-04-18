a5823f628ed1f9dbf0d122d3e4cfa06d713a66f0
Pharylon <me@zachshuford.com>
2015-04-17 22:17:41 -0400

    Starting on lighting the pins

--- clock.py
+++ clock.py
@@ -1,4 +1,8 @@
-print("Hello import datetime
+import datetime
+import RPi.GPIO
+
+ypins = [17, 18, 27, 22, 23, 24, 25]
+xpins = [5, 6, 12]
 
 def setArray(myInt, array):
 	asBinary = "{0:b}".format(myInt).zfill(7)
@@ -8,6 +12,14 @@ def setArray(myInt, array):
 		else:
 			array[i] = True
 
+
+for i in xpins:
+    GPIO.setup(i, GPIO.OUT)
+    GPIO.output(i, False)
+
+for i ypins:
+    GPIO.setup(i, GPIO.IN)
+    GPIO.output(i, False)
 	
 grid = [[0 for x in range(7)] for x in range(3)] 
 
@@ -15,6 +27,10 @@ now = datetime.datetime.now()
 setArray(now.hour, grid[0])
 setArray(now.minute, grid[1])
 setArray(now.second, grid[2])
+
+for i in range(0, 7):
+    for j in range(0, 3):
+        
 
 print(len(grid))
 print(grid)
