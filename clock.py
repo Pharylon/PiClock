7f784b4ca3778bc1ffc9718c27e46c37ef53398c
Pharylon <me@zachshuford.com>
2015-04-18 14:41:22 +0000

    Working now

--- clock.py
+++ clock.py
@@ -1,5 +1,10 @@
 import datetime
-import RPi.GPIO
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+#GPIO.cleanup()
 
 ypins = [17, 18, 27, 22, 23, 24, 25]
 xpins = [5, 6, 12]
@@ -14,23 +19,37 @@ def setArray(myInt, array):
 
 
 for i in xpins:
-    GPIO.setup(i, GPIO.OUT)
-    GPIO.output(i, False)
-
-for i ypins:
     GPIO.setup(i, GPIO.IN)
-    GPIO.output(i, False)
+    #GPIO.output(i, False)
+
+for i in ypins:
+    GPIO.setup(i, GPIO.IN)
+    #GPIO.output(i, False)
 	
 grid = [[0 for x in range(7)] for x in range(3)] 
 
-now = datetime.datetime.now()
-setArray(now.hour, grid[0])
-setArray(now.minute, grid[1])
-setArray(now.second, grid[2])
+'''
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
+GPIO.output(17, False)
+GPIO.output(5, True)
 
-for i in range(0, 7):
-    for j in range(0, 3):
-        
+time.sleep(1)
+'''
 
-print(len(grid))
-print(grid)
+while True:
+    now = datetime.datetime.now()
+    setArray(now.hour, grid[0])
+    setArray(now.minute, grid[1])
+    setArray(now.second, grid[2])
+    for i in range(0, 7):
+        for j in range(0, 3):
+            if (grid[j][i]):
+                GPIO.setup(xpins[j], GPIO.OUT)
+                GPIO.setup(ypins[i], GPIO.OUT)
+                GPIO.output(xpins[j], True)
+                GPIO.output(ypins[i], False)
+                GPIO.setup(xpins[j], GPIO.IN)
+                GPIO.setup(ypins[i], GPIO.IN)
+
+GPIO.cleanup()
